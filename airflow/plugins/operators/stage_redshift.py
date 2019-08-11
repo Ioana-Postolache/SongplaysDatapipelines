95fdea679326fa4a321283a0465c1dba178fc03d
Ioana-Postolache <ioana.postolache.zaharia@gmail.com>
2019-08-11 10:09:50 +0000

    Added functionality for creating stage tables

--- airflow/plugins/operators/stage_redshift.py
+++ airflow/plugins/operators/stage_redshift.py
@@ -4,23 +4,27 @@ from airflow.utils.decorators import apply_defaults
 
 class StageToRedshiftOperator(BaseOperator):
     ui_color = '#358140'
+    stage_sql_template = """
+    DROP TABLE IF EXISTS {destination_table};
+    {create_table_sql};
+    """
 
     @apply_defaults
     def __init__(self,
-                 # Define your operators params (with defaults) here
-                 # Example:
-                 # redshift_conn_id=your-connection-name
+                 # Define your operators params (with defaults) 
+                 redshift_conn_id="",
+                 destination_table="",
+                 create_table_sql="",
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
-        # Map params here
-        # Example:
-        # self.conn_id = conn_id
+        # Map params
+        self.redshift_conn_id = redshift_conn_id
+        self.create_table_sql = create_table_sql
+        self.destination_table = destination_table
 
     def execute(self, context):
-        self.log.info('StageToRedshiftOperator not implemented yet')
-
-
-
-
-
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+        stage_sql = StageToRedshiftOperator.stage_sql_template.format(destination_table = self.destination_table, create_table_sql = self.create_table_sql)
+        redshift.run(stage_sql)
+        self.log.info(f"StageToRedshiftOperator - created stage table: {destination_table}")
